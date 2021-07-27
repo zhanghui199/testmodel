12790954308d584ec14722768bb4399894cca9e6
fas_zhanghui <397243131@qq.com>
2021-07-27 14:49:10 +0800

    引用变量

---
+++ testlibrary/src/main/java/com/lzx/testlibrary/TestUtils.java
@@ -0,0 +1,15 @@
+package com.lzx.testlibrary;
+
+/**
+ * <pre>
+ *     author : 张辉
+ *     e-mail : 397243131@qq.com
+ *     time   : 2021/07/27
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class TestUtils {
+
+    public final static String text = "依赖包测试";
+}
