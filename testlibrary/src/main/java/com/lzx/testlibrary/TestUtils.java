19b9770d916ec0e053a01391f4ff13d077e41db4
fas_zhanghui <397243131@qq.com>
2021-07-27 14:53:58 +0800

    引用变量

--- testlibrary/src/main/java/com/lzx/testlibrary/TestUtils.java
+++ testlibrary/src/main/java/com/lzx/testlibrary/TestUtils.java
@@ -11,5 +11,16 @@ package com.lzx.testlibrary;
  */
 public class TestUtils {
 
-    public final static String text = "依赖包测试";
+    public final static String text = "依赖包测试1";
+
+    public final static String text2 = "依赖包测试2";
+
+    public final static String text3 = "依赖包测试3";
+
+
+    public final static String text4 = "依赖包测试4";
+
+
+
+
 }
